5563ff44bc926f36ee2f9f6bdabe19474ef6fd6b
MarlonDav <marlon.bernal.riveros@gmail.com>
2023-02-27 14:10:01 -0500

    fiiin

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -2,13 +2,11 @@
 const BASE_URL = process.env.BASE_URL
 
 export default async function handler (req, res) {
-  console.log(req.body)
   const { value } = req.body
 
   const response = await fetch(`${BASE_URL}/${value}`)
     .then(res => res.json())
     .then(res => {
-      console.log(res)
       return res
     })
   res.status(200).json(response)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,43 @@
 import Head from 'next/head'
-import { API_URL } from '@/utils/CONST.js'
+import Image from 'next/image'
+// https://nextjs.org/docs/advanced-features/module-path-aliases
 import { useState } from 'react'
+import Definition from '@/components/Definition'
+
 export default function Home () {
-  console.log(API_URL)
-  const [value, setValue] = useState('')
-  const [result, setResult] = useState({})
-  const handledSearch = () => {
+  const [result, setResult] = useState(null)
+
+  const handledSarh = (e) => {
+    e.preventDefault()
+    const form = new FormData(e.target)
+    const value = form.get('value')
+
     fetch('/api/hello', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ value })
+      body: JSON.stringify({ value, peito: 'hola' })
     })
       .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setResult(result)
-      })
+      .then(data => setResult(data))
   }
+
+  const [font, setFont] = useState('font-montserrat')
+  const handleChangeFont = (font) => {
+    if (font === 'quicksand') {
+      setFont('font-quicksand')
+    }
+
+    if (font === 'montserrat') {
+      setFont('font-montserrat')
+    }
+
+    if (font === 'inter') {
+      setFont('font-inter')
+    }
+  }
+
   return (
     <>
       <Head>
@@ -27,20 +46,46 @@ export default function Home () {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Diccionario</h1>
-        <input placeholder="Enter to word"
-        onChange={(e) => {
-          setValue(e.target.value)
-        }}/>
-        <button onClick={() => handledSearch()}>
+      <main className={font}>
+        <header>
+          <Image src="/logobook.png" alt="logo" height={60} width={60} onClick={() => window.location.reload()}/>
+          {/* <h1>Diccionario</h1> */}
+          <select className={font} onChange={(e) => handleChangeFont(e.target.value)}>
+            <option value='montserrat' className="font-montserrat">Montserrat</option>
+            <option value='inter' className="font-inter">Inter</option>
+            <option value='quicksand' className="font-quicksand">Quicksand</option>
+          </select>
+        </header>
+        <form onSubmit={handledSarh}>
+          <input
+            placeholder="Enter to world"
+            name="value"
+          />
+          <button className='search-button' type="submit">
             Buscar
-        </button>
-        {
-          JSON.stringify(result)
-        }
-        
-      </main>
+          </button>
+        </form>
+        <div>
+            {
+              Array.isArray(result)
+                ? <Definition result={result} />
+                : null
+            }
+            {/* {
+              result.length > 0 && <Definition result={result} />
+            } */}
+            {
+              !result && (
+                <div className='result_empty'>
+                <h2>
+                  Add a word to search
+                </h2>
+              </div>
+              )
+            }
+        </div>
+        </main>
+        <footer></footer>
     </>
   )
 }
